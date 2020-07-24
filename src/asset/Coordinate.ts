61314103c3bae990847b8b88349a28686af0375f
Jin IGARASHI <wushilan0623@gmail.com>
2020-07-24 11:52:37 +0900

    deleted utm coordinates from the sample. it has never used them

--- src/asset/Coordinate.ts
+++ src/asset/Coordinate.ts
@@ -5,8 +5,6 @@ export default class Coordinate {
   lon: number;
   lat: number;
   elevation: number;
-  lon_utm: number;
-  lat_utm: number;
   demand: number;
   pattern: string;
   isJunction: boolean;
@@ -16,8 +14,6 @@ export default class Coordinate {
     this.lon = roundTo(params.lon, 6);
     this.lat = roundTo(params.lat, 6);
     this.elevation = roundTo(params.elevation, 0);
-    this.lon_utm = roundTo(params.lon_utm, 3);
-    this.lat_utm = roundTo(params.lon, 3);
     this.demand = params.demand ? roundTo(params.demand, 6) : 0;
     this.pattern = params.patten ? params.patten : '';
     this.isJunction = true;
